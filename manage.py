ef713d2888cb996e29561bd2f59ef9f821de1bba
Benjymack <newzealandbenjy@gmail.com>
2022-06-28 21:53:43 +1200

    Added road stuff

--- manage.py
+++ manage.py
@@ -6,6 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    if os.getenv('DJANGO_SETTINGS_MODULE') is None:
+        raise RuntimeError('DJANGO_SETTINGS_MODULE environment variable is '
+                           'not set.') 
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
