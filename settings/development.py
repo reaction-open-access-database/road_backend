b693b7a3b141d4a0216ee5393903c78c7f328363
Benjymack <newzealandbenjy@gmail.com>
2022-08-26 18:18:42 +1200

    Added email support

--- settings/development.py
+++ settings/development.py
@@ -9,3 +9,9 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_PORT = os.environ['EMAIL_PORT']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_USE_TLS = True
