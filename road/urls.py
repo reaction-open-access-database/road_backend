54901abc848d9b80f8bad74b9776dabf8dc7d15f
Benjymack <newzealandbenjy@gmail.com>
2022-08-30 10:29:36 +1200

    Added ability to resend verification email

--- road/urls.py
+++ road/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import MoleculeViewSet, UserViewSet
-from dj_rest_auth.registration.views import VerifyEmailView, RegisterView, ConfirmEmailView
+from dj_rest_auth.registration.views import VerifyEmailView, RegisterView, ConfirmEmailView, ResendEmailVerificationView
 from dj_rest_auth.views import LoginView, LogoutView
 from rest_framework_simplejwt.views import TokenVerifyView
 from dj_rest_auth.jwt_auth import get_refresh_view
@@ -15,6 +15,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('logout/', LogoutView.as_view()),
+    path('resend-email-verification/', ResendEmailVerificationView.as_view()),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('token/refresh/', get_refresh_view().as_view(), name='token_refresh'),
     path('account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
