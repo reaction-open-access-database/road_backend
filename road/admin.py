44e78754e02b81d4933eef8816a3fb213d97ce8a
Benjymack <newzealandbenjy@gmail.com>
2022-08-30 18:09:03 +1200

    Added Molecule, Reaction, ReactionComponent and ReactionSource to admin interface

--- road/admin.py
+++ road/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Molecule, Reaction, ReactionComponent, \
+    ReactionSource
 
 
 admin.site.register(UserProfile)
+
+admin.site.register(Molecule)
+admin.site.register(Reaction)
+admin.site.register(ReactionComponent)
+admin.site.register(ReactionSource)
