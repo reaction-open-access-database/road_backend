465d3d0ad1e9fe1c3e3d1fabe38ab3c3c588e74d
Benjy <newzealandbenjy@gmail.com>
2022-09-29 11:35:37 +1300

    Fixed issue with creating user

--- road/models.py
+++ road/models.py
@@ -69,4 +69,4 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance, owner=instance)
+        UserProfile.objects.create(owner=instance)
