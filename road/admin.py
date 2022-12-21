4ae16946905b1edce86f0322a45eb325b2606c07
Benjy <newzealandbenjy@gmail.com>
2022-12-22 12:46:12 +1300

    Added the name and molecular formula fields to the admin page for Molecule.

--- road/admin.py
+++ road/admin.py
@@ -5,7 +5,11 @@ from .models import UserProfile, Molecule, Reaction, ReactionComponent, \
 
 admin.site.register(UserProfile)
 
-admin.site.register(Molecule)
 admin.site.register(Reaction)
 admin.site.register(ReactionComponent)
 admin.site.register(ReactionSource)
+
+
+@admin.register(Molecule)
+class MoleculeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'molecular_formula')
