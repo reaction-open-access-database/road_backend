4b28f7d18fc486d1a40e22a58f11806846b2bf2b
Benjy <newzealandbenjy@gmail.com>
2022-12-26 18:19:51 +1300

    Added environment variables to GitHub Action

--- settings/test.py
+++ settings/test.py
@@ -4,8 +4,6 @@ Settings for the testing environment
 
 import os
 
-os.environ["SECRET_KEY"] = "test-environment-secret-key"
-
 os.environ["EMAIL_HOST"] = "example.com"
 os.environ["EMAIL_PORT"] = "25"
 os.environ["EMAIL_HOST_USER"] = "user"
