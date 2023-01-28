431d91af39cecc525a9c49558e44cc5e83563887
Benjy <newzealandbenjy@gmail.com>
2023-01-29 09:48:57 +1300

    Made test environment variables defaults, not replacements

--- settings/test.py
+++ settings/test.py
@@ -4,16 +4,16 @@ Settings for the testing environment
 
 import os
 
-os.environ["EMAIL_HOST"] = "example.com"
-os.environ["EMAIL_PORT"] = "25"
-os.environ["EMAIL_HOST_USER"] = "user"
-os.environ["EMAIL_HOST_PASSWORD"] = "password"
-os.environ["EMAIL_FROM"] = "user@example.com"
-
-os.environ["ADMIN_URL"] = "admin/"
-os.environ["LOGIN_URL"] = "login/"
-
-os.environ["ALLOWED_HOST"] = "localhost"
+os.environ = {
+    "EMAIL_HOST": "example.com",
+    "EMAIL_PORT": "25",
+    "EMAIL_HOST_USER": "user",
+    "EMAIL_HOST_PASSWORD": "password",
+    "EMAIL_FROM": "user@example.com",
+    "ADMIN_URL": "admin/",
+    "LOGIN_URL": "login/",
+    "ALLOWED_HOST": "localhost",
+} | os.environ
 
 # If this is a development environment, load secret key, database, etc from .env file
 from dotenv import find_dotenv, load_dotenv  # pylint: disable=wrong-import-position
